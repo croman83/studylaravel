22cf1edede44e904ea4d544445644503c8f6cdb7
Roman <casian.roma@gmail.com>
2017-10-09 20:35:54 +0300

    old

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,19 @@
-@extends('admin.layouts.app')
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <!--    CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-
+    <!-- Styles -->
+    <link href="{{ mix('css/admin.css') }}" rel="stylesheet">
+</head>
+<body>
 
 
 
@@ -60,4 +70,10 @@
             </div>
         </div>
     </form>
-@endsection
+
+    @if (getenv('APP_ENV') === 'local')
+        <script id="__bs_script__">//<![CDATA[
+            document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.6'><\/script>".replace("HOST", location.hostname));
+            //]]></script>
+    @endif
+</body>
